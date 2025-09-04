73fddd0e865f7588478e7189fa2ec3c729384f75
PeurDeLaConduite <contact.peurdelaconduite@gmail.com>
2025-09-04 11:48:24 +0200

    Feat: configure package aliases

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     experimental: {},
+    transpilePackages: [
+        "@packages/ui",
+        "@packages/types",
+        "@packages/services",
+        "@packages/domain",
+    ],
 
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
